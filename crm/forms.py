d72df645db5b1e71301771faddbb3f95f2edc3c0
Inafs <Klunk5@yandex.ru>
2022-07-14 23:14:01 +0300

    init

---
+++ crm/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class OrderForm(forms.Form):
+    name = forms.CharField(max_length=200)
+    phone = forms.CharField(max_length=200)
+    mail = forms.CharField(max_length=200, required=False)
+    text = forms.TextField(max_length=1000, required=False)
