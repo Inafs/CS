6df878e5f235e8f23fdc56115d409fef3abb9e4f
Inafs <Klunk5@yandex.ru>
2022-07-22 19:08:54 +0300

    finish

--- pages/views.py
+++ pages/views.py
@@ -5,10 +5,19 @@ from fos.forms import OrderForm
 # Create your views here.
 def first_page(request):
     sliders = Slider.objects.filter(page=2)
+    
+    index_slider = Slider.objects.get(page=2, title_slider="Слайдер для главной")
+    end_slider = Slider.objects.get(page=2, title_slider="Нижний Слайдер")
+    
+    sliders = sliders.exclude(title_slider="Нижний Слайдер")
+    sliders = sliders.exclude(title_slider="Слайдер для главной")
+    
     desc = Description.objects.filter(page=2)
     form = OrderForm(request.POST or None)
 
     context = {
+        'index_slider': index_slider,
+        'end_slider': end_slider,
         'sliders': sliders,
         'form': form,
         'desc': desc,
